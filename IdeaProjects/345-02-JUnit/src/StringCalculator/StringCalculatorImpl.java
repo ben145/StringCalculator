b3baf4e8f94164ba0d845d087cf7a17c86073952
Ben Hamula <bhamula1@ithaca.edu>
2018-01-30 14:12:24 -0500

    Implementation, Part 1

---
+++ IdeaProjects/345-02-JUnit/src/StringCalculator/StringCalculatorImpl.java
@@ -0,0 +1,14 @@
+package StringCalculator;
+
+/**
+ * Created by Ben on 1/29/2018.
+ */
+public class StringCalculatorImpl implements StringCalculator {
+
+    @Override
+    public int Add(String numbers) {
+        if (numbers.length() < 1)
+            return 0;
+        return -1;
+    }
+}
