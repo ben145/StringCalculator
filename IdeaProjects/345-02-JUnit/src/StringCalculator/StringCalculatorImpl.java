419bdaf606c80de0be022f8a6f9b4babdc658490
Ben Hamula <bhamula1@ithaca.edu>
2018-01-30 16:31:27 -0500

    Implementation, Part 3

--- IdeaProjects/345-02-JUnit/src/StringCalculator/StringCalculatorImpl.java
+++ IdeaProjects/345-02-JUnit/src/StringCalculator/StringCalculatorImpl.java
@@ -9,6 +9,22 @@ public class StringCalculatorImpl implements StringCalculator {
     public int Add(String numbers) {
         if (numbers.length() < 1)
             return 0;
-        return -1;
+        boolean justOne = true;
+        for (int i = 0; i < numbers.length(); i++) {
+            if (numbers.charAt(i) == ',')
+                justOne = false;
+        }
+        if (justOne)
+            return Integer.parseInt(numbers);
+        else {
+            int commaIndex = 0;
+            for (int i = 0; i < numbers.length(); i++) {
+                if (numbers.charAt(i) == ',')
+                    commaIndex = i;
+            }
+            int s1 = Integer.parseInt(numbers.substring(0, commaIndex));
+            int s2 = Integer.parseInt(numbers.substring(commaIndex+1, numbers.length()));
+            return s1 + s2;
+        }
     }
 }
